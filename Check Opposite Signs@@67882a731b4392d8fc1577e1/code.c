e36a6174fe8f720784dace6d16e55d87524a563d
Gurnoor05 <itzsekhongurnoor05@gmail.com>
2025-02-05 16:54:33 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -11,11 +11,11 @@ void main()
     {
         printf("False");
     }
-    else if(-a&&0 || a&&0)
+    else if(-a&&b==0 || a&&b==0)
     {
         printf("False");
     }
-    else if(-b&&0 || b&&0)
+    else if(-b&&a==0 || b&&a==0)
     {
         printf("False");
     }
